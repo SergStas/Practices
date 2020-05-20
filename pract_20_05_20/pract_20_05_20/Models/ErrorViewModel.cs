f00c743130db2d3cc7d4cb5dc40c5d4c2e459138
SergStas <sergstas0@gmail.com>
2020-05-20 21:30:17 +0500

    05/20 practice - project created

---
+++ pract_20_05_20/pract_20_05_20/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pract_20_05_20.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
