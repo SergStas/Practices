4729c6f1b5561ad769bde255ad28d7b73984c127
SergStas <sergstas0@gmail.com>
2020-05-23 14:23:10 +0500

    05/23 practice

---
+++ pract_23_05_20/fixed/encoder/FileEncoder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace encoder
+{
+    public class FileEncoder
+    {
+        public void EncodeToFile(string filePath) 
+        {
+            byte[] content = File.ReadAllBytes(filePath);
+            string encoded = Base64Encode(content);
+            string[] parts = filePath.Split('\\');
+            string name = parts[parts.Length - 1].Replace(".", "_") + ".txt";
+            string resultPath = string.Join("\\", parts.Take(parts.Length - 1)) + "\\" + name;
+            File.WriteAllText(resultPath, encoded);
+        }
+
+        public void DecodeFromFile(string filePath)
+        {
+            string content = File.ReadAllText(filePath);
+            byte[] decoded = Base64Decode(content);
+            File.WriteAllBytes(GetDecodedFilePath(filePath), decoded);
+        }
+
+        public void EncodeToBuffer(string filePath)
+        {
+            byte[] content = File.ReadAllBytes(filePath);
+            string encoded = Base64Encode(content);
+            Clipboard.SetText(encoded);
+        }
+
+        public void DecodeFromBuffer(string filePath)
+        {
+            string content = Clipboard.GetText();
+            byte[] decoded = Base64Decode(content);
+            File.WriteAllBytes(filePath, decoded);
+        }
+
+        string Base64Encode(byte[] bytes) =>
+            Convert.ToBase64String(bytes);
+
+        byte[] Base64Decode(string base64EncodedData) =>
+            Convert.FromBase64String(base64EncodedData);
+
+        string GetDecodedFilePath(string filePath)
+        {
+            string path = string.Join(".", filePath.Split('.').Take(filePath.Split('.').Length - 1));
+            string[] parts = path.Split('_');
+            return string.Join("_", parts.Take(parts.Length - 1)) + "0." + parts[parts.Length - 1];
+        }
+    }
+}
---
+++ pract_23_05_20/fixed/encoder/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Windows.Forms;
+using System.Text;
+using System.IO;
+using System;
+using System.Linq;
+using encoder;
+
+namespace pract_23_05_20
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            FileEncoder encoder = new FileEncoder();
+            while (true)
+            {
+                try
+                {
+                    string operation = Console.ReadLine();
+                    if (operation == "-quit" || operation == "-q")
+                        break;
+                    string source = Console.ReadLine();
+                    if (!(source.Split(' ')[0] == "-s" || source.Split(' ')[0] == "-source") || source.Split(' ').Length != 2)
+                    {
+                        Console.WriteLine("Incorrect input");
+                        continue;
+                    }
+                    string path = Console.ReadLine();
+                    if (!(path.Split(' ')[0] == "-f" || path.Split(' ')[0] == "-file") || path.Split(' ').Length != 2)
+                    {
+                        Console.WriteLine("Incorrect input"); 
+                        continue;
+                    }
+                    if (operation == "-encode" || operation == "-e")
+                    {
+                        if (source.Split(' ')[1] == "file")
+                            encoder.EncodeToFile(path.Split(' ')[1]);
+                        else if (source.Split(' ')[1] == "buffer")
+                            encoder.EncodeToBuffer(path.Split(' ')[1]);
+                        else
+                        {
+                            Console.WriteLine("Incorrect input");
+                            continue;
+                        }
+                    }
+                    else if (operation == "-decode" || operation == "-d")
+                    {
+                        if (source.Split(' ')[1] == "file")
+                            encoder.DecodeFromFile(path.Split(' ')[1]);
+                        else if (source.Split(' ')[1] == "buffer")
+                            encoder.DecodeFromBuffer(path.Split(' ')[1]);
+                        else
+                        {
+                            Console.WriteLine("Incorrect input");
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input");
+                        continue;
+                    }
+                    Console.WriteLine("Successfully");
+                }
+                catch
+                {
+                    Console.WriteLine("Something wrong");
+                }
+            }
+        }
+    }
+}
