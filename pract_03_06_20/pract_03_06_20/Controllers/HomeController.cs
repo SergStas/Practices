e11b1d6a79acc41ddf3b02e7548693135bf7b055
SergStas <sergstas0@gmail.com>
2020-06-04 21:05:39 +0500

    pract 06/03

---
+++ pract_03_06_20/pract_03_06_20/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using pract_03_06_20.Models;
+
+namespace pract_03_06_20.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() =>
+            View();
+
+        public IActionResult ChessPage() =>
+            View();
+
+        public IActionResult Privacy() =>
+            View();
+
+        public bool CheckStep(string arg) =>
+            ChessLogic.CheckStep(arg);
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
---
+++ pract_03_06_20/pract_03_06_20/Models/ChessLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+namespace pract_03_06_20.Models
+{
+    public static class ChessLogic
+    {
+        public static bool CheckStep(string arg)
+        {
+            string state = arg.Split('_')[0],
+                sFrom = arg.Split('_')[1],
+                sTo = arg.Split('_')[2];
+            Point from = new Point(int.Parse(sFrom[1].ToString()), sFrom[0] - 'A');
+            Point to = new Point(int.Parse(sTo[1].ToString()), sTo[0] - 'A');
+            switch (state)
+            {
+                case "p": return Pawn(from, to);
+                case "b": return Bishop(from, to);
+                case "r": return Rook(from, to);
+                case "n": return Knight(from, to);
+                case "q": return Queen(from, to);
+                case "k": return King(from, to);
+                default: return false;
+            }
+        }
+
+        private static bool Pawn(Point from, Point to) =>
+            from.X == to.X && (from.Y - to.Y == 2 || from.Y - to.Y == 1);
+
+        private static bool Bishop(Point from, Point to) =>
+            Math.Abs(from.X - to.X) == Math.Abs(from.Y - to.Y) && from.X != to.X;
+
+        private static bool Rook(Point from, Point to) =>
+            from.X == to.X ^ from.Y == to.Y;
+
+        private static bool Queen(Point from, Point to) =>
+            Rook(from, to) || Bishop(from, to);
+
+        private static bool King(Point from, Point to) =>
+            (Math.Abs(from.X - to.X) <= 1 || Math.Abs(from.Y - to.Y) <= 1) && Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y) != 0;
+
+        private static bool Knight(Point from, Point to) =>
+            Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y) == 3 && from.X != to.X && from.Y != to.Y;
+    }
+}
