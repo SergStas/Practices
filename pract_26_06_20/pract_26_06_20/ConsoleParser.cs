d06e2eacecfb3dd4fb00288f4ffafc89eaead38a
SergStas <sergstas0@gmail.com>
2020-06-26 21:37:29 +0500

    Fraction task

---
+++ pract_26_06_20/pract_26_06_20/ConsoleParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pract_26_06_20
+{
+    public class ConsoleParser
+    {
+        private Dictionary<char, Func<Ratio, Ratio, Ratio>> _operations;
+        
+        public void Start()
+        {
+            InitializeDictionary();
+            bool quit = false;
+            while (!quit)
+                ParseLine(out quit);
+        }
+
+        private void InitializeDictionary() =>
+            _operations = new Dictionary<char, Func<Ratio, Ratio, Ratio>>
+            {
+                {'+', (ratio, ratio1) => ratio + ratio1},
+                {'-', (ratio, ratio1) => ratio - ratio1},
+                {'*', (ratio, ratio1) => ratio * ratio1},
+                {':', (ratio, ratio1) => ratio / ratio1},
+            };
+
+        private void ParseLine(out bool quit)
+        {
+            quit = false;
+            string request = Console.ReadLine().Replace("//", ":");
+            if (request == "quit")
+                quit = true;
+            else
+            {
+                string[] parts = request.Split(new[] {':', '-', '+', '*'});
+                Ratio result = _operations[request[parts[0].Length]](ParseRatio(parts[0]), ParseRatio(parts[1]));
+                Console.WriteLine(result);
+            }
+        }
+
+        private Ratio ParseRatio(string s)
+        {
+            string[] parts = s.Split(' ');
+            int integer = parts.Length == 1 ? 0 : int.Parse(parts[0]);
+            string[] ratioParts = parts[^1].Split('/');
+            return new Ratio(int.Parse(ratioParts[0]), int.Parse(ratioParts[1]), integer);
+        }
+    }
+}
---
+++ pract_26_06_20/pract_26_06_20/Ratio.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace pract_26_06_20
+{
+    public class Ratio
+    {
+        public int Upper
+        {
+            get => _upper;
+            set
+            {
+                _upper = value;
+                Normalize();
+            }
+        }
+        public int Lower
+        {
+            get => _lower;
+            set
+            {
+                _lower = value;
+                Normalize();
+            }
+        }
+        public int Integer
+        {
+            get => _integer;
+        }
+
+        private int _lower;
+        private int _upper;
+        private int _integer;
+
+        public Ratio(int upper, int lower)
+        {
+            _upper = Math.Abs(upper);
+            Lower = lower;
+        }
+
+        public Ratio(int upper, int lower, int integer)
+        {
+            _integer = integer;
+            _upper = Math.Abs(upper);
+            Lower = lower;
+        }
+
+        private void Normalize()
+        {
+            _upper += _lower * _integer;
+            _integer = _upper / _lower;
+            _upper %= _lower;
+            int common = GetCommon(_upper, _lower);
+            _upper /= common;
+            _lower /= common;
+        }
+
+        private static int GetCommon(int f, int s) => f == 0 ? s : GetCommon(s % f, f);
+
+        public static Ratio operator +(Ratio r1, Ratio r2) =>
+            r1.Add(r2);
+
+        public static Ratio operator -(Ratio r1, Ratio r2) =>
+            r1.Sub(r2);
+
+        public static Ratio operator *(Ratio r1, Ratio r2) =>
+            r1.Mp(r2);
+
+        public static Ratio operator /(Ratio r1, Ratio r2) =>
+            r1.Div(r2);
+
+        public Ratio Add(Ratio ratio)
+        {
+            if (Lower != ratio.Lower)
+                throw new NotImplementedException();
+            Ratio result = new Ratio(Upper + ratio.Upper, Lower);
+            return result;
+        }
+
+        public Ratio Sub(Ratio ratio)
+        {
+            if (Lower != ratio.Lower)
+                throw new NotImplementedException();
+            Ratio result = new Ratio(Upper - ratio.Upper, Lower);
+            return result;
+        }
+
+        public Ratio Mp(Ratio ratio) =>
+            new Ratio(Upper * ratio.Upper, Lower * ratio.Lower);
+
+        public Ratio Div(Ratio ratio) =>
+            new Ratio(Upper * ratio.Lower, Lower * ratio.Upper);
+
+        public override string ToString() =>
+            Integer == 0 && Upper == 0 ? "0" : $"{(Integer == 0 ? "" : $"{Integer} ")}{(Upper == 0 ? "" : $"{Upper}/{Lower}")}";
+    }
+}
